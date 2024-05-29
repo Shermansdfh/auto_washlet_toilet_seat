2f7d67c8b98cfd1ee74cffcdf5e8dc4afdffc5e5
Sherman <tw.sherman.chang@gmail.com>
2024-05-29 17:01:56 +0800

    Remove get_gesture function to avoid repeat reading of gesture value.

--- toilet_seat_main/toilet_seat_main.ino
+++ toilet_seat_main/toilet_seat_main.ino
@@ -4,13 +4,17 @@
 #include <Arduino_APDS9960.h>
 #include <ezButton.h>
 #include <Arduino.h>
-#include <cmath>
-#include <map>
+// #include <cmath>
+// #include <map>
+
+#define DEBUG
 
 /******** Define Pin & Create Module Object ********/
-#define VRX_PIN  A13 
+#define VRX_PIN  A15
 #define VRY_PIN  A14
-#define SW_PIN   A15
+#define SW_PIN   A13
+
+ezButton button(SW_PIN);
 
 void setup() {
 	Serial.begin(9600);
@@ -31,11 +35,9 @@ void setup() {
 int prevJoystickValueX  = 0, prevJoystickValueY = 0;
 bool isPumpingWater = false;
 bool isJoystickMovingX = false, isJoystickMovingY = false;
-ezButton button(SW_PIN);
 /******** End Initialize Variable ********/
 
 /******** Declare Function Prototype ********/
-int get_gesture();
 /******** End Declare Function Prototype ********/
 
 void loop() {
@@ -69,30 +71,34 @@ void loop() {
      */
 
 	// Check if the joystick is released on the X-axis
-	if (std::abs(prevJoystickValueX - 512) < 10) {
+	if (abs(joystickValueX - 512) < 50) {
 		isJoystickMovingX = false;  // Stop servo movement
+		// Serial.println(joystickValueX);
 	} else {
 		isJoystickMovingX = true;   // Start servo movement
 	}
 
-	if (std::abs(prevJoystickValueY - 512) < 10) {
+	if (abs(joystickValueY - 512) < 50) {
 		isJoystickMovingY = false;  // Stop servo movement
+		// Serial.println(joystickValueY);
 	} else {
 		isJoystickMovingY = true;   // Start servo movement
 	}
 
 	// Update the servo position if the joystick is not in the middle position
 	if (isJoystickMovingX) {
-		int servoAngle = std::map(joystickValueX, 0, 1023, 0, 180);  // Map joystick value to servo angle
+		int servoAngle = map(joystickValueX, 0, 1023, 0, 180);  // Map joystick value to servo angle
 		tube_controller.tubeRotate(servoAngle);                             // Set the servo position
 		prevJoystickValueX = joystickValueX;                     // Update the previous joystick value
 	}
 
 	if (isJoystickMovingY) {
-		int servoAngle = std::map(joystickValueY, 0, 1023, 0, 180);  // Map joystick value to servo angle
+		int servoAngle = map(joystickValueY, 0, 1023, 0, 180);  // Map joystick value to servo angle
 		tube_controller.tubeForward(servoAngle);                             // Set the servo position
 		prevJoystickValueY = joystickValueY;                     // Update the previous joystick value
 	}
+
+	// Serial.println(joystickValueB);
 
 	/******* End Tube Control *******/
 
@@ -101,26 +107,35 @@ void loop() {
 	 * This section handles the control and operations related to the seat.
      */
 
-    int currentGesture = get_gesture();
+	if (APDS.gestureAvailable()) {
+    	// a gesture was detected, read and print to Serial Monitor
+		int gesture = APDS.readGesture();
 
-    if (currentGesture == GESTURE_RIGHT) {
-		Serial.println("APDS: Right gesture detected");
-        seat_opener.openFirstLayer();
-    } else if (currentGesture == GESTURE_LEFT) {
-		Serial.println("APDS: Left gesture detected");
-        seat_opener.openSecondLayer();
-    } else if (currentGesture == GESTURE_UP) {
-		Serial.println("APDS: Up gesture detected");
-        seat_opener.closeAll();
-    }
+		switch (gesture) {
+			case GESTURE_UP:
+				Serial.println("APDS: Up gesture detected");
+				seat_opener.closeAll();
+				break;
+
+			case GESTURE_DOWN:
+				Serial.println("APDS: Down gesture detected");
+				break;
+
+			case GESTURE_LEFT:
+				Serial.println("APDS: Left gesture detected");
+				seat_opener.openSecondLayer();
+				break;
+
+			case GESTURE_RIGHT:
+				Serial.println("APDS: Right gesture detected");
+				seat_opener.openFirstLayer();
+				break;
+
+			default:
+				// ignore
+				break;
+		}
+	}
 
 	/******* End Seat Control *******/
-}
-
-
-int get_gesture()  {
-	if (APDS.gestureAvailable()) {
-		int gesture = APDS.readGesture();
-        return gesture;
-  	}
 }
